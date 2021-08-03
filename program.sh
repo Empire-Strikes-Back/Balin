926e1e83aa806456b54d038fcefb53c16c0a74aa
cloud-scout <cloud-scout@program>
2021-08-03 20:03:15 +0300

    i revert "i use deps-repl library for :repl"
    This reverts commit 471071eec5d62aa9a538da4eb74b4deee2e10289.

--- program.sh
+++ program.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 repl(){
-  clj \
-    -X:repl deps-repl.core/process \
-    :main-ns cloud-scout.main \
-    :port 7788 \
-    :host '"0.0.0.0"' \
-    :repl? true \
-    :nrepl? false
+  clj -A:repl
 }
 
 "$@"
